6f993ad9c1fc1562e9c5493807bb48d10f8577e4
Amanuel <amanuelalemayehuseta@gmail.com>
2026-09-03 17:13:28 +0300

    Added function for printing slot machine result

--- slot_machine.py
+++ slot_machine.py
@@ -35,7 +35,13 @@ def get_slot_machine_spin(rows, cols, symbols):
     return columns
 
 
-
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], "|")
+            else:
+                print(column[row])
 
 
 def deposit():
@@ -98,6 +104,9 @@ def main():
 
 
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 
 
